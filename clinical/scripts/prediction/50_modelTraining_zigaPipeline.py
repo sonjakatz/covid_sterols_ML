38881840e348836c42d07c1a17948add90f88ce0
Sonja Katz <sonja.katz@wur.nl>
2023-03-31 12:10:36 +0200

    pushed clinical and T1 scripts+results

---
+++ clinical/scripts/prediction/50_modelTraining_zigaPipeline.py
@@ -0,0 +1,177 @@
+''' 
+Start remotely to run in the background like this:
+
+ssh skatz@212.235.237.86 -p 62636 'source miniconda3/bin/activate sklearn | /home/skatz/miniconda3/envs/sklearn/bin/python /home/skatz/PROJECTS/covid/disease_severity/discoveryValidation/clinical/scripts/prediction/50_modelTraining_zigaPipeline.py'
+'''
+
+
+import os
+import pandas as pd
+import numpy as np
+import json
+import matplotlib.pyplot as plt 
+import seaborn as sns
+import random
+import sys
+
+from classification_ziga import classify_leave_one_out_cv
+from preprocessing import imputation_scaling, SupervisedSelector
+
+sns.set_theme(style="whitegrid", palette=None, font_scale=1.2)
+
+from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
+from sklearn.svm import SVC
+from sklearn.gaussian_process import GaussianProcessClassifier
+
+from sklearn.model_selection import train_test_split 
+from sklearn.model_selection import GridSearchCV
+from sklearn import metrics
+from sklearn.metrics import RocCurveDisplay
+from sklearn.metrics import classification_report, confusion_matrix
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.neural_network import MLPClassifier
+from sklearn.preprocessing import StandardScaler
+from sklearn.naive_bayes import GaussianNB
+from sklearn.discriminant_analysis import QuadraticDiscriminantAnalysis
+from sklearn.dummy import DummyClassifier 
+
+from sklearn.gaussian_process.kernels import RBF
+from sklearn.gaussian_process.kernels import DotProduct
+from sklearn.gaussian_process.kernels import Matern
+from sklearn.gaussian_process.kernels import RationalQuadratic
+from sklearn.gaussian_process.kernels import WhiteKernel
+
+import warnings
+warnings.filterwarnings('ignore')
+
+PATH_base = "/home/skatz/PROJECTS/covid/disease_severity/discoveryValidation/clinical/"
+
+''' Prepare data '''
+datasetTimepoint = "12_uponAdmission"
+target = "disease_severity"
+#### Feature selection:
+vars = "boruta"   
+varPath = f"{PATH_base}/results/featureSelection/{datasetTimepoint}/{vars}.txt"
+
+''' 
+Define paths
+'''
+resultsPath = f"{PATH_base}/results/prediction/featureSelection/{datasetTimepoint}/{vars}/modelComparison/zigaPipeline"
+os.makedirs(resultsPath, exist_ok=True)
+dataPath = f"{PATH_base}/results/preprocessing/cleaned"
+dataset = f"{datasetTimepoint}_{target}_cleaned.csv"
+
+models = {
+      #    'svc': SVC(probability=True),
+          'rfc': RandomForestClassifier(),  
+      #     'gpr': GaussianProcessClassifier(),
+      #     'abc': AdaBoostClassifier(base_estimator = DecisionTreeClassifier(random_state = 11, max_features = "auto", class_weight = "balanced",max_depth = None)),
+      #     'log': LogisticRegression(),
+      #     'knn': KNeighborsClassifier(),
+      #     'mlp': MLPClassifier(),
+      #     'gnb': GaussianNB(),
+      #     'qda': QuadraticDiscriminantAnalysis(),
+      #     'mcl': DummyClassifier(strategy="most_frequent"),
+         }               
+
+
+grids = {'rfc':{
+                'n_estimators': [100, 300, 1000],      ### changed
+               'max_depth': [2,4,6],         
+               'max_features': [2,4,6],  
+               'ccp_alpha':  list(np.linspace(0, 0.025, 2)),   
+               },
+         'svc':{'C': [0.1, 1, 10, 100],  
+                'gamma': [1, 0.1, 0.01, 0.001, 0.0001], 
+                'kernel': ['rbf', 'poly', 'linear']     
+               },
+         'gpr':{'kernel':[1*RBF(), 1*DotProduct(), 1*Matern(),  1*RationalQuadratic(), 1*WhiteKernel()]},
+         'abc':{"base_estimator__criterion" : ["gini", "entropy"],
+                "base_estimator__splitter" :   ["best", "random"],
+                "n_estimators": [1, 2]
+               },
+         'log':{'penalty': ['l1','l2'], 'C': [0.001,0.01,0.1,1,10,100,1000]},     
+         'knn':{'n_neighbors': list(range(1, 15)),
+               'weights': ['uniform', 'distance'],
+               'metric': ['euclidean', 'manhattan']},
+         'mlp':{'solver': ['adam'],    
+                'max_iter': [50, 100, 200],        
+                'alpha': 10.0 ** -np.arange(0, 5),            
+                'hidden_layer_sizes': [(random.randrange(15, 41), random.randrange(5, 16)) for i in range(5)],           
+               },  
+         'gnb': {'var_smoothing': np.logspace(-9,9, num=100)},
+         'qda': {'reg_param': (0.00001, 0.0001, 0.001,0.01, 0.1), 
+                 'store_covariance': (True, False),
+                 'tol': (0.0001, 0.001,0.01, 0.1)},
+         'mcl': {},  
+         }   
+
+''' 
+Read data
+'''
+data = pd.read_csv(f"{dataPath}/{dataset}", index_col=0)
+X = data.drop(target, axis=1)
+y = data[target]
+
+##### FOR DEVELOPMENT PURPOSES: smaller dataset
+# X = X.iloc[:5,:]
+# y = y[:5]
+
+''' 
+Read in variables
+'''
+sel_variables = pd.read_csv(varPath, header=None)[0].tolist()
+
+''' 
+Impute & scale X (according to Sonja's pipeline)
+only works like this with LOOCV (no MICE possible on test split!)
+'''
+num_columns = X.select_dtypes(include=["float64"]).columns
+bin_columns = X.select_dtypes(include=["int64"]).columns
+cat_columns = X.select_dtypes(include=["object"]).columns
+preprocessor = imputation_scaling(num_columns, bin_columns, cat_columns, X)     
+X_imputed = preprocessor.fit_transform(X)
+X_imputed = SupervisedSelector(preprocessor, sel_variables).transform(X_imputed)
+
+''' 
+Run Pipeline
+'''
+for model in models.keys():
+      df_before = pd.DataFrame()    
+      df_features = pd.DataFrame()
+      df_importances = pd.DataFrame() 
+
+      saveIndivdualPred = True
+
+      clf = GridSearchCV(models[model], grids[model], scoring='balanced_accuracy', verbose=1, cv=3, n_jobs=-1)  ##cv=5
+      result = classify_leave_one_out_cv(clf, 
+                              X_imputed, 
+                              y,
+                              model=model, 
+                              save_to = resultsPath + f"/{model}", 
+                              select_features=True, 
+                              permutation_repeats=100, 
+                              scale_features = False,
+                              saveIndivdualPred = saveIndivdualPred,                              
+                              logfile = "log_clinical")
+      ''' Prepare results '''
+      result['model'] = model        
+      df_before = df_before.append(result['df_results'], ignore_index=True)  
+      df_importances = df_importances.append(result['importances_df'], ignore_index=True)    
+      if saveIndivdualPred:
+            df_indPred = pd.DataFrame()
+            df_indPred = df_indPred.append(result["df_indPred"], ignore_index=True)
+
+      del result['df_results']      
+      del result['importances_df']  
+      del result['df_indPred'] 
+      df_features = df_features.append(result, ignore_index=True)  
+
+
+      ''' Save to file '''
+      df_before.to_csv((resultsPath+f"/prediction_cv_test_{model}.csv"), index=False)          
+      df_features.to_csv((resultsPath+f"/features_test_{model}.csv"), index=False)  
+      df_importances.to_csv((resultsPath+f"/importances_test_{model}.csv"), index=False)                                 
+      df_indPred.to_csv((resultsPath+f"/individualPredictions_test_{model}.csv"), index=False)            
---
+++ clinical/scripts/prediction/classification_ziga.py
@@ -0,0 +1,180 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import random
+import re
+import sys
+
+from sklearn import svm, datasets
+from sklearn.metrics import auc, roc_curve
+from sklearn.metrics import RocCurveDisplay  
+from sklearn.metrics import accuracy_score, f1_score, precision_score, recall_score, classification_report, confusion_matrix, balanced_accuracy_score
+from sklearn.model_selection import StratifiedKFold, cross_val_score,cross_val_predict, LeaveOneOut
+
+from sklearn.metrics import confusion_matrix  
+from sklearn.inspection import permutation_importance
+from sklearn.preprocessing import StandardScaler, OrdinalEncoder    
+from sklearn.impute import KNNImputer  
+
+def reduce_features(fitted_model, 
+					X,
+					y,
+					eps=0, 
+					permutation_repeats = 20): 
+	
+	df_imps = pd.DataFrame()
+	df_imps['feature'] = X.columns
+
+	sel_features_all = {}
+	for i in range(permutation_repeats):
+		imps = permutation_importance(fitted_model, X, y,)
+		df_imps[f'importance_{i}'] = imps.importances_mean
+		
+		sel_features = df_imps.loc[df_imps[f'importance_{i}'] > eps, 'feature'].values
+
+		for f in sel_features:
+			if f in sel_features_all:
+				sel_features_all[f] += 1
+			else:
+				sel_features_all[f] = 1   
+
+	sel_features = []
+	for f,n in sel_features_all.items():
+		if n >= permutation_repeats//2:
+			sel_features.append(f)
+	
+	return sel_features, df_imps   
+
+def scale(X_train, X_validate, scale_features = True):
+	if scale_features:
+		scaler = StandardScaler()
+		scaler = scaler.fit(X_train) 
+
+		X_validate = pd.DataFrame(scaler.transform(X_validate), columns = X_validate.columns)  
+
+	return X_validate     
+		   
+
+def classify_leave_one_out_cv(clf, X, y, model="", save_to="", select_features=True, scale_features = True, title = "", logfile="log", saveIndivdualPred = True,**kwargs):
+	# #####################################    
+	# Classification  
+
+	# Run classifier with leave-one-out cross-validation   
+	try:
+		kf = LeaveOneOut()
+
+		df_results = pd.DataFrame()  	
+		
+		all_y = []
+		all_probs =  []
+		all_predicts=[]
+
+		if select_features:
+			selected_features_ind = {}
+			importances_df = pd.DataFrame()  
+
+		for i, (train, test) in enumerate(kf.split(X, y)):
+		
+			X_train = X.iloc[train].copy()
+			y_train = y.iloc[train].copy()
+
+			with open(f'{logfile}.out', 'a') as f:
+				f.write(f"Split: {i+1} / {len(X)}; Model: {model}\n")
+			
+
+			X_test = X.iloc[test] # no need to copy, only a signle element
+			y_test = y.iloc[test].iloc[0] # no need to copy, only a signle element
+			
+			if scale_features:
+				#https://stackoverflow.com/questions/38058774/scikit-learn-how-to-scale-back-the-y-predicted-result
+				scalerX = StandardScaler().fit(X_train)
+				X_train[:] = scalerX.transform(X_train)
+				X_test[:] = scalerX.transform(X_test)
+
+
+			clf.fit(X_train, y_train) #hyperparameter tuning
+			
+			if select_features: # permutation feature selection
+				sel_features, imps = reduce_features(clf, 
+													X_train,
+													y_train,
+													**kwargs) 
+				
+				importances_df = pd.concat([importances_df, imps])         
+
+				if sel_features:  
+					for sf in sel_features: 
+						# individual features based analysis
+						if sf in selected_features_ind:
+							selected_features_ind[sf] += 1
+						else:
+							selected_features_ind[sf] = 1 
+
+			y_pred = clf.predict(X_test)  
+			y_predProba = clf.predict_proba(X_test)[:,1]
+
+			all_y.append(y_test)
+			all_probs.append(y_predProba)
+			all_predicts.append(y_pred[0])   
+		
+		all_y = np.array(all_y)
+		all_probs = np.array(all_probs)
+		all_predicts = np.array(all_predicts)
+
+		tn, fp, fn, tp = confusion_matrix(all_y, all_predicts).ravel()
+		prec = (tp/(tp+fp))
+		rec = (tp/(tp+fn))
+		f1 = (tp/(tp+0.5*(fp+fn)))
+		acc = ((tp+tn)/(tp+tn+fp+fn))
+
+		# roc and auc
+		fpr, tpr, _ = roc_curve(all_y,all_probs)
+
+		auc_val = auc(fpr, tpr)
+		# plt.plot(fpr, tpr, lw=2, alpha=0.5, label='LOOCV ROC (AUC = %0.2f)' % (auc_val))
+		# plt.plot([0, 1], [0, 1], linestyle='--', lw=2, color='k', label='Chance level', alpha=.8)
+		# plt.xlim([-0.05, 1.05])
+		# plt.ylim([-0.05, 1.05])
+		# plt.xlabel('False Positive Rate')
+		# plt.ylabel('True Positive Rate')
+		# plt.title(model)
+		# plt.legend(loc="lower right")
+		# plt.show()
+
+		results = {"precision": prec,
+			"recall": rec,
+			"f1": f1,
+			"accuracy": acc,
+			"model": model,
+			"auc": auc_val,
+			}  
+		df_results = df_results.append(results, ignore_index=True)  
+
+		if select_features: 
+			# individual features based analysis
+			final_features_ind = []
+			for sf, n in selected_features_ind.items():
+				if n >= len(X)//2:
+					final_features_ind.append(sf)
+			final_features_ind.sort() 
+
+		final_results = {}
+		final_results['df_results'] = df_results 
+
+
+		if select_features:
+			final_results['selected_features_ind'] = final_features_ind
+			importances_df = importances_df.assign(model=model)   
+			final_results['importances_df'] = importances_df       
+
+		if saveIndivdualPred:
+			df_indPred = pd.DataFrame(np.column_stack((all_y, all_probs, all_predicts)), columns=["y_true", "y_predProb", "y_pred"])
+			final_results["df_indPred"] = df_indPred
+				 
+	except Exception as e:
+		with open(f'{logfile}.out', 'a') as f:
+			f.write(f"\n\n{str(e)}\n")		
+		return 
+	return final_results
+
+
